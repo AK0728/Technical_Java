966f00072ef269ef932d4d423bfeb5254ff5faf1
Avish Kumar <120946981+AK0728@users.noreply.github.com>
2024-02-04 20:18:03 +0000

    completed task

---
+++ sumDouble/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main
+{
+    static int sumDouble(int x, int y){
+        return x != y ? x+y : 2*(x+y);
+    } 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 2 numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = sumDouble(a, b);
+        System.out.println("The Result is = "+result);
+        sc.close();
+    }
+}
