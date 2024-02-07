5b654b2209b20317ce832c72c732acbd6d5565d8
Avish Kumar <120946981+AK0728@users.noreply.github.com>
2024-02-07 19:08:06 +0000

    completed

---
+++ Makes10/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main
+{
+static boolean makes10(int a, int b){
+    return (a == 10)||(b == 10)||(a + b == 10)? true: false;
+    }
+ public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       System.out.print("Enter 1st integer: ");
+       int a= sc.nextInt();
+       System.out.print("Enter 2nd integer: ");
+       int b= sc.nextInt();
+    
+       System.out.println(makes10(a,b));
+       sc.close();
+
+    }
+
+}
