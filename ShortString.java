9203956a237741b1eec2f81fb52ed3914fc7c6b4
Avish Kumar <120946981+AK0728@users.noreply.github.com>
2024-08-29 18:46:18 +0000

    completed ShortString.java

---
+++ ShortString.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ShortString {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string:");
+        String str = sc.nextLine();
+        System.out.println("The new short string is "+shortenString(str));
+
+        sc.close();
+
+    }
+    public static String shortenString(String s){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            int count = 1;
+            for(int j=i+1; j<s.length(); j++){
+                if(s.charAt(i) == s.charAt(j)){
+                    count++;              
+                }
+                else
+                    break;
+            }
+            
+            sb.append(s.charAt(i));
+            sb.append(count);
+            i = i+count-1;
+        }
+        return sb.toString();
+    }
+}
